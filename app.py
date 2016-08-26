ffd0a25d500069f28316de7dfa5974530c20286e
Gus Cavanaugh <Gus Cavanaugh>
2016-08-26 08:25:24 -0400

    updated dashboard

--- app.py
+++ app.py
@@ -9,35 +9,25 @@ def create_app():
 
 app = create_app()
 
-"""
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    print "running"
-    myDFs, rooms, years = create_pivots('Traffic Report Sample_gus.xlsx', 'Traffic Data')
-    if request.method == 'POST':
-        if request.form['btn'] == 'display':
-            yr = request.form['year']
-            rm = request.form['room']
-            print yr, rm
-            for myDF in myDFs:
-                if myDF['room'] == int(rm) and myDF['year'] == int(yr):
-                    myFiltDF = myDF['df']
-                    return render_template('index.html', myFoo=myFiltDF.to_html(), years=years, rooms=rooms)
-            return render_template('index.html', years=years, rooms=rooms, no_data_msg="No Data for that Unit & Year")
-    return render_template('index.html', years=years, rooms=rooms)
-"""
-
 @app.route("/", methods=['GET', 'POST'])
 def test():
     df = pd.read_excel('Traffic Report Sample_gus.xlsx')
-    filt_df = df[df['Year'] == 2016]
-    pivot_df = pd.pivot_table(filt_df, values='nameid', columns='Action', index=['rmpropid','mrktname'], aggfunc='count')
-    pivot_df.reset_index(inplace=True)
-    pivot_df.index.name = None
-    cdf = pd.pivot_table(df, columns='Action', index=['rmpropid', 'leasagid'], values='nameid', aggfunc='count', margins=True)
-    #cdf.columns = [' '.join(col).strip() for col in cdf.columns.values]
-    cdf.reset_index(inplace=True)
-    return render_template('report.html', myDF=pivot_df.to_html(), cdf=cdf.to_html())
+    df_2015 = df[df['Year'] == 2015]
+    df_2016 = df[df['Year'] == 2016]
+    props_2015 = df_2015.rmpropid.tolist()
+    props_2016 = df_2016.rmpropid.tolist()
+    years = set(df.Year.tolist())
+    if request.method == 'POST':
+        if request.form['btn'] == 'display':
+            prop = request.form['room']
+            year = request.form['year']
+            year_df = df[df['Year'] == int(year)]
+            filt_df = year_df[year_df['rmpropid'] == int(prop)]
+            pivot_df = pd.pivot_table(filt_df, values='nameid', columns='Action', index='mrktname', aggfunc='count')
+            cdf = pd.pivot_table(filt_df, columns='Action', index='leasagid', values='nameid', aggfunc='count', margins=True)
+            tdf = pd.pivot_table(df, values='nameid', index='Month', aggfunc='count')
+            return render_template('report.html', myDF=pivot_df.to_html(), cdf=cdf.to_html(), tdf=tdf.to_html())
+    return render_template('index.html', years=years, props_2015=props_2015, props_2016=props_2016)
 
 
 if __name__ == "__main__":
